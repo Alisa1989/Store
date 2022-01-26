52ed9e041e903194e0b329fad4f1bdb873b42ab5
Codelleschi <a.steinhauslin@gmail.com>
2022-01-25 20:43:05 -0800

    added redux store with axios call

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import {productsReducer} from './state/reducers/productsReducer'; 
+import thunk from "redux-thunk";
+
+const store = createStore(productsReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
