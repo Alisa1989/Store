e15766870af3fa417741240b569a187afaf3a19b
Codelleschi <a.steinhauslin@gmail.com>
2022-02-14 12:58:15 -0800

    added header with cart animation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import LandingContainer from './components/pages/landing/LandingContainer';
 import CartContainer from './components/pages/cart/CartContainer';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <LandingContainer />
         <CartContainer />
       </header>
---
+++ src/components/common/Header.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+
+function Header(props) {
+
+    const [cartIcon, setCartIcon] = useState(false);
+   
+    useEffect(() => {
+        console.log("cartIcon", cartIcon)
+        setCartIcon(true)
+        setTimeout(() => {
+            setCartIcon(false)
+        }, 2000)
+    }, [props.cart])
+
+
+    const renderCartButton = () => {
+            if (cartIcon) {
+                return <AddShoppingCartIcon  />
+            } else {
+                 return <ShoppingCartIcon />
+             } 
+    }
+
+    return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Store64
+          </Typography>
+            <IconButton
+               size="large"
+               edge="start"
+               color="inherit"
+               aria-label="open drawer"
+               sx={{ mr: 2 }}
+               >
+               {renderCartButton()}
+             </IconButton>
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+const mapStateToProps = (state) => {
+    return {
+      cart: state.cart
+    };
+  };
+
+export default connect(mapStateToProps, null)(Header);
