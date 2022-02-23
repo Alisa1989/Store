dec855a3fbe58349585dd9b16eaf466dbf925ee2
Codelleschi <a.steinhauslin@gmail.com>
2022-02-23 10:32:21 -0800

    added productPage

---
+++ src/components/pages/productPage/ProductContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductInformation from './ProductInformation';
+
+const ProductContainer = () => {
+    return (
+        <>
+        ProductContainer
+        <ProductInformation/>
+        </>
+    )
+}
+
+export default ProductContainer;
---
+++ src/components/pages/productPage/ProductInformation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductInformation = () => {
+    return (
+        <>
+            ProductInfo
+        </>
+    )
+}
+
+export default ProductInformation;
