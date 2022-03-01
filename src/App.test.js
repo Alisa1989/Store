4f2695552da0aa856e0c3cb5eab9e22025a7395f
Codelleschi <a.steinhauslin@gmail.com>
2022-02-28 19:47:17 -0800

    added some tests and made the cart routing more accessible

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from "redux";
@@ -7,12 +7,34 @@ import rootReducer from './state';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-test('renders app withour errors', () => {
+test('renders app without errors', () => {
     render(<Provider store={store}><App/></Provider>);
+  });
 
+test('renders the app header', ()=> {
+  //Arrange
+    render(<Provider store={store}><App/></Provider>);
+  //Act
+    const header = screen.getByText(/Store64/i)    
+  });
+
+test('logo has link to home', async ()=> {
+  //Arrange
+    render(<Provider store={store}><App/></Provider>);
+  //Act
+    const logo = screen.getByText(/Store64/i)    
+    expect(logo.closest('a')).toHaveAttribute('href', '/')
+  });
+
+test('cart has link to cart', async ()=> {
+  //Arrange
+    render(<Provider store={store}><App/></Provider>);
+  //Act
+    const logo = screen.getByText(/go to cart/i)    
+    expect(logo.closest('a')).toHaveAttribute('href', '/cart')
+  });
   
   //arrange
   //act
   //assert
   //test false positives
-});
