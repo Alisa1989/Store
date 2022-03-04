872a8c9dcea7dd5dba21f2029942fcfe0f456152
Codelleschi <a.steinhauslin@gmail.com>
2022-03-04 15:19:31 -0800

    added refactoring for product title

--- src/components/common/ProductCard.js
+++ src/components/common/ProductCard.js
@@ -8,6 +8,19 @@ import { Button, CardActionArea, CardActions } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function ProductCard(props) {
+  //reduces length of title so they can be consistent
+  //the string is cut off afte the last word within the lenght limit
+  const maxLength = (text, length) => {
+    if (text.length > length) {
+      text = text.substring(0,length-1);
+      console.log("text before", text);
+      const lastSpace = text.lastIndexOf(" ");
+      text = text.substring(0,lastSpace) + "...";
+      console.log("text after", text);
+
+    }
+    return text;
+  }
   return (
     <Card class="product-card" sx={{ maxWidth: 345 }}>
       <Link to={`/${props.item.id}`} style={{textDecoration: "none"}}>
@@ -20,11 +33,11 @@ function ProductCard(props) {
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div" style={{color:"black"}}>
-              {props.item.title}
+              {maxLength(props.item.title, 50)}
             </Typography>
-            <Typography variant="body2" color="text.secondary" style={{color:"black"}} sx={{height: 200}} >
+            {/* <Typography variant="body2" color="text.secondary" style={{color:"black"}} sx={{height: 200}} >
               {props.item.description}
-            </Typography>
+            </Typography> */}
           </CardContent>
         </CardActionArea>
       </Link>
