60a57a92bb0c1b4418354576b0e4bfae95c699fc
Codelleschi <a.steinhauslin@gmail.com>
2022-07-27 20:46:38 -0700

    changed title truncation on card

--- src/components/common/ProductCard.js
+++ src/components/common/ProductCard.js
@@ -8,16 +8,6 @@ import { Button, CardActionArea, CardActions } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function ProductCard(props) {
-  //reduces length of title so they can be consistent
-  //the string is cut off afte the last word within the lenght limit
-  const maxLength = (text, length) => {
-    if (text.length > length) {
-      text = text.substring(0,length-1);
-      const lastSpace = text.lastIndexOf(" ");
-      text = text.substring(0,lastSpace) + "...";
-    }
-    return text;
-  }
   return (
     <Card className="product-card" sx={{ maxWidth: 345}} style={{border: "none", boxShadow: "none"}}>
       <Link to={`/customer/product${props.item.id}`} style={{textDecoration: "none"}}>
@@ -32,8 +22,8 @@ function ProductCard(props) {
             />
           </div>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div" style={{color:"black"}} sx={{height: 100}} >
-              {maxLength(props.item.title, 40)}
+            <Typography gutterBottom variant="h5" component="div" style={{color:"black", overflow: "hidden"}} sx={{height: 100}} >
+              {props.item.title}
             </Typography>
             <Typography variant="body2" color="text.secondary" style={{color:"black"}} md={{height: 10}} sx={{fontWeight: 'bold', fontSize: 20}}>
               ${props.item.price}
